7f7323c9577f6591cfa472b1a4528b49b61e1578
newfreewaylam <newfreeway.alam@gmail.com>
2026-02-22 19:50:34 +0800

    Create astro.config.mjs

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // 這裡可以設定你的網域，例如 site: 'https://newfreeway.mo'
+  outDir: './dist',
+  publicDir: './public',
+});
